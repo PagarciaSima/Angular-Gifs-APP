aa13f55b99fa967fe9b21f971d4dfa9e73d4324a
CORP\pagarcia <pablo.garcia-simavillal@capgemini.com>
2024-10-16 20:25:02 +0200

    Diseño gifs component

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gif-shearch-box',
+  template: `
+    <h5>Buscar:</h5>
+    <input type="text" class="form-control" placeholder="Buscar gifs...">
+  `
+})
+
+export class SearchBoxComponent {
+  constructor() { }
+}
