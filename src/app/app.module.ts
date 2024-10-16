45f26406086210389bfaeef2a767d7958b63b6c7
CORP\pagarcia <pablo.garcia-simavillal@capgemini.com>
2024-10-16 19:33:37 +0200

    Estructura inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { GifsModule } from './gifs/gifs.module';
+import { NgModule } from '@angular/core';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    GifsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
